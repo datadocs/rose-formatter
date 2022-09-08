c040474fe0fb25d9cb9ff187990e33fb47b3a599
Alexandre Felipe <o.alexandre.felipe@gmail.com>
2022-09-08 16:15:45 +0100

    Embed SVG images in the readme.

--- scripts/freeze-readme.js
+++ scripts/freeze-readme.js
@@ -2,32 +2,55 @@ const fs = require('fs');
 const https = require('https');
 const readme_file = __dirname + '/../README.md'
 let README = fs.readFileSync(readme_file, {encoding:'utf-8'})
-let FROZEN_SECTION = /\<\!--\s*BEGIN\s*FROZEN\s*IMAGE\s*(https:\/\/\S*?)(?:\s*as\s*(\S*?))?\s*-->.*?\<!--\s*END\s*FROZEN\s*IMAGE\s*--\>/igs
-const UPDATED_README = README.replace(FROZEN_SECTION, downloadAndUpdate);
-fs.writeFileSync(readme_file, UPDATED_README)
-console.log(UPDATED_README)
+const FROZEN_SECTION = /\<\!--\s*BEGIN\s*FROZEN\s*IMAGE\s*(https:\/\/\S*?)(?:\s*as\s*(\S*?))?\s*-->.*?\<!--\s*END\s*FROZEN\s*IMAGE\s*--\>/igs
+const FROZEN_RE = /\!\[]\((static\/frozen-[^)]*)\)/g
+
+const pending_downloads = []
+README = README.replace(FROZEN_SECTION, downloadAndUpdate);
+Promise.all(pending_downloads).then(
+  r => {
+    README = README.replace(FROZEN_RE, embedFrozenImage)
+  }
+).then(r => {
+  fs.writeFileSync(readme_file, README)
+})
 function downloadAndUpdate(body, url, alias){
   if(!alias){
     alias = url.split('?')[0].split('/').pop()
   }
-  https.get(url, 
-  {
-    "headers": {
-      "cache-control": "no-cache",
-      "pragma": "no-cache"
+  
+  pending_downloads.push(new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Timeout')), 15000)
+    https.get(url, 
+    {
+      "headers": {
+        "cache-control": "no-cache",
+        "pragma": "no-cache"
+      },
     },
-  }, (response, error) => {
-  if(error){
-    console.log(`Failed to download ${url}`)
-  }else{
-    const file = fs.createWriteStream(`${__dirname}/../static/frozen-${alias}`);
-    file.on('finish', () => {
-      console.log(`Saved file ${alias}`)
+    (response, error) => {
+      if(error){
+        console.log(`Failed to download ${url}`)
+        reject(new Error(`Failed to download ${url}`))
+      }else{
+        const file = fs.createWriteStream(`${__dirname}/../static/frozen-${alias}`);
+        file.on('finish', () => {
+          console.log(`Saved file ${alias}`)
+          resolve();
+        })
+        response.pipe(file);
+      }
     })
-    response.pipe(file);
-  }
-});
+ }))
  return `<!-- BEGIN FROZEN IMAGE ${url} as ${alias} -->
-  <img src="./static/frozen-${alias}">
+  ![](static/frozen-${alias})
 <!-- END FROZEN IMAGE -->`
+}
+
+function embedFrozenImage(_, img){
+  if(img.toLowerCase().endsWith('.svg')){
+    return fs.readFileSync(`${__dirname}/../${img}`, {encoding:'utf-8'})
+  }else{
+    return _;
+  }
 }
